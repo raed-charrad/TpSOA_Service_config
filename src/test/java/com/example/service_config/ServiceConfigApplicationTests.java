213a255024021a0919d09ccca39f3200c38734d2
raed charrad <raed.charrad91@gmail.com>
2022-10-19 10:31:45 +0100

    first commit

---
+++ src/test/java/com/example/service_config/ServiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.service_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
